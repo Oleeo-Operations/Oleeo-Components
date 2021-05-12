3cfb43dc0df31689e753d603c75279375c773491
Matthew  Hawes <Matthew.hawes@OFFICE.WCN.CO.UK>
2021-05-12 09:07:51 +0100

    Fix: changing cache expiry time

--- src/services/cache-service.ts
+++ src/services/cache-service.ts
@@ -17,7 +17,7 @@ class CacheService {
   private readonly cache: Map<string, CacheEntry> = new Map();
 
   // The expiry time (in ms) of the cache. I.e. CacheEntries older than this force a new request.
-  private readonly cacheExpiry = 1000 * 60 * 60 * 8; // 8 Hours;
+  private readonly cacheExpiry = 1000 * 60 * 60 * 1; // 8 Hours;
 
   /**
    * Retrieve an entry from the cache. If not found in memory, check in the browser's localStorage.
